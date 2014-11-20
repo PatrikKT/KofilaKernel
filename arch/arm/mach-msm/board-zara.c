e434f016b4f84d5add3f7622a395436158034619
Patrik Zakhar <123patokt@gmail.com>
2014-11-20 09:14:34 +0100

    Kexec-hardboot patch

--- arch/arm/mach-msm/board-zara.c
+++ arch/arm/mach-msm/board-zara.c
@@ -1096,6 +1096,17 @@ static int msm8930_change_memory_power(u64 start, u64 size,
 
 static void __init msm8930_allocate_memory_regions(void)
 {
+#ifdef CONFIG_KEXEC_HARDBOOT
+	// Reserve space for hardboot page at the end of first system ram block
+	struct membank* bank = &meminfo.bank[0];
+	phys_addr_t start = bank->start + bank->size - SZ_1M;
+	int ret = memblock_remove(start, SZ_1M);
+	if(!ret)
+		pr_info("Hardboot page reserved at 0x%X\n", start);
+	else
+		pr_err("Failed to reserve space for hardboot page at 0x%X!\n", start);
+#endif
+
 	zara_allocate_fb_region();
 }
 
